a71ccc336724092d3f0779c50dc088a0392fdeda
br8dley <73848085+br8dley@users.noreply.github.com>
2021-04-13 15:11:27 -0500

    Updated file
    created and updated file.

---
+++ main.js
@@ -0,0 +1,15 @@
+//myAge = 13;
+const myAge = 13;
+//earlyYears
+let earlyYears = 3;
+earlyYears *= 1;
+let laterYears = myAge - 4;
+laterYears *=3;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Bradley".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} in dog years.  `);
